27f7650c1d015acb27949c28b0020a494cdc0cfe
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-28 14:16:10 -0600

    Added LibraryUsers package and LibraryUser interface

---
+++ src/com/teksystems/InClassPractice.java
@@ -0,0 +1,12 @@
+package com.teksystems;
+
+public class InClassPractice {
+
+    public static void practice () {
+
+
+
+    }
+
+
+}
---
+++ src/com/teksystems/LibraryUsers/LibraryUser.java
@@ -0,0 +1,6 @@
+package com.teksystems.LibraryUsers;
+
+public interface LibraryUser {
+    public void registerAccount();
+    public void requestBook();
+}
