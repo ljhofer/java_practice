68e2e145ce468fc9d9d3b8c532be71b32f79bdab
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-02-28 14:29:54 -0600

    Added KidUser class

---
+++ src/com/teksystems/LibraryUsers/KidUser.java
@@ -0,0 +1,31 @@
+package com.teksystems.LibraryUsers;
+
+public class KidUser implements LibraryUser{
+
+    // fields
+    public int age;
+    public String bookType;
+
+    @Override
+    public void registerAccount() {
+        if (age <= 12) {
+            System.out.println("You have successfully registered under a Kids account");
+        } else if (age >= 12) {
+            System.out.println("Sorry, Age must be less than 12 to register as a kid");
+        }
+    }
+
+    @Override
+    public void requestBook() {
+        if (bookType == "Kids") {
+            System.out.println("Book issued successfully, please return the book within 10 days");
+        } else {
+            System.out.println("Oops, you are allowed to take only kids books");
+        }
+
+    }
+
+
+
+
+}
