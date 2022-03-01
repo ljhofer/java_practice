23d07adc4f52d808512d435b43015b3175463e5c
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-03-01 16:42:15 -0600

    Added solution for exercises 1 and 2

---
+++ src/com/teksystems/OperatorsAndNumbers.java
@@ -0,0 +1,22 @@
+package com.teksystems;
+
+public class OperatorsAndNumbers {
+
+    // exercise 1
+        // 1
+        // 1000
+        // 0010 0001
+        // 0100 1110
+        // 0011 0001 0011
+        // 1000 0100 1100 0011
+
+    // exercise 2
+        // 2
+        // 9
+        // 52
+        // 114
+        // 543
+        // 11367
+
+
+}
