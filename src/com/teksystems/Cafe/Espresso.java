60bf862403927a079a4bf602bfa1a13b639ace81
MARKLAPTOP\Laura <ljhofer@gmail.com>
2022-03-04 16:10:33 -0600

    Added espresso class

---
+++ src/com/teksystems/Cafe/Espresso.java
@@ -0,0 +1,46 @@
+package com.teksystems.Cafe;
+
+public class Espresso extends Product {
+
+    boolean extraShot = false;
+    boolean macchiato = false;
+
+    public Espresso(String name, double price, String description, boolean extraShot, boolean macchiato) {
+        super(name, price, description);
+        this.extraShot = false;
+        this.macchiato= false;
+    }
+
+    public Espresso() {
+        super("Espresso", 3.75, "Guaranteed to wake you up!");
+        this.extraShot = false;
+        this.macchiato= false;
+    }
+
+    public boolean isExtraShot() {
+        return extraShot;
+    }
+
+    public void setExtraShot(boolean extraShot) {
+        this.extraShot = extraShot;
+    }
+
+    public boolean isMacchiato() {
+        return macchiato;
+    }
+
+    public void setMacchiato(boolean macchiato) {
+        this.macchiato = macchiato;
+    }
+
+    @Override
+    public double calculateProductTotal(Integer userQuantity) {
+        double addShot = (this.extraShot ? 2.00 : 0);
+        double addMacchiato = (this.macchiato ? 1.00 : 0);
+        double subTotal = getPrice()*userQuantity + addShot + addMacchiato;
+
+        return subTotal;
+    }
+
+
+}
